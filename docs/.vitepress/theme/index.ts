ed80e482a6eb1c3e3b0d7aa44f815693ff6faf0b
XL <lixiang020317@gmail.com>
2024-11-25 15:23:54 +0800

    docs: change theme color

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,8 @@
 import { OlButton } from 'onionl-ui'
-
 import 'uno.css'
-import './custom.css'
-import './var.css'
-
 // // 如果你使用默认主题
 import DefaultTheme from 'vitepress/theme'
+import './custom.css'
 
 export default { extends: DefaultTheme, enhanceApp({ app }) {
   app.component('OlButton', OlButton)

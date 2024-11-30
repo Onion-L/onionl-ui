0a0cba5706c1feb8754398e9f75a1237cc496f2b
XL <lixiang020317@gmail.com>
2024-12-01 00:48:07 +0800

    chore: update

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,15 +1,8 @@
-import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
-  resolve: {
-    alias: {
-      'onionl-ui': resolve(__dirname, '../packages/components'),
-      '@onionl-preset': resolve(__dirname, 'preset'),
-      '@onionl-ui/utils': resolve(__dirname, '../packages/utils'),
-    },
-  },
+
 })

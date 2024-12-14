7fc519865babe48b20961b907976f7b8170430d3
Onion-L <onionl5236@gmail.com>
2024-12-14 11:16:19 +0800

    wip: pixelImage component MVP

---
+++ packages/components/pixelImage/src/pixelImage.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import type { Ref } from 'vue'
+import { computed, onMounted, ref, useAttrs } from 'vue'
+import { pixelImageCreator } from './pixelImage'
+
+defineOptions({
+  name: 'OlPixelImage',
+  inheritAttrs: false,
+})
+
+const attrs = useAttrs()
+
+const filterAttrs = computed(() => {
+  const { src } = attrs
+  return { src: src as string }
+})
+
+const imgRef = ref<HTMLImageElement>()
+
+onMounted(() => {
+  if (imgRef.value) {
+    pixelImageCreator(imgRef as Ref<HTMLImageElement>)
+  }
+})
+</script>
+
+<template>
+  <img ref="imgRef" v-bind="filterAttrs" class="pixel-image" width="300" height="auto">
+</template>
--- play/src/App.vue
+++ play/src/App.vue
@@ -1,50 +1,14 @@
-<script setup>
-import { OlContextMenu, OlContextMenuItem } from 'onionl-ui'
-
-// const isVisible = ref(false)
-// const clientX = ref(0)
-// const clientY = ref(0)
-
-// const contextMenuRef = ref()
-
-// window.addEventListener('contextmenu', (e) => {
-//   e.preventDefault()
-//   console.log('contextmenu')
-//   console.log(e.clientX, e.clientY)
-//   isVisible.value = true
-
-//   nextTick(() => {
-//     clientY.value = e.clientX
-//     clientY.value = e.clientY
-//     contextMenuRef.value.style.left = e.clientX
-//     console.log(contextMenuRef.value.style.left)
-//   })
-// })
-
-// window.addEventListener('click', () => {
-//   clientY.value = 100
-//   console.log('click', clientX.value)
-//   isVisible.value = false
-// })
+<script setup lang="ts">
+import { OlPixelImage } from 'onionl-ui'
 </script>
 
 <template>
   <div class="p-4">
-    <h1 class="text-2xl font-bold color-white">
-      Context Menu
+    <h1 class="text-2xl font-bold">
+      Pixel Image
     </h1>
-    <OlContextMenu>
-      <OlContextMenuItem>label</OlContextMenuItem>
-    </OlContextMenu>
-
-    <div class="fixed min-w-100px min-h-2 bg-gray-800 color-white border-1 border-solid border-gray-300 rounded-md shadow-lg">
-      <ul class=" list-none p-0 m-y-2">
-        <li
-          class="cursor-pointer py-2 text-left flex items-center justify-center space-x-2 hover:bg-gray-700 transition-colors duration-150"
-        >
-          <span>label</span>
-        </li>
-      </ul>
+    <div class="parent">
+      <ol-pixel-image src="/vue.png" />
     </div>
   </div>
 </template>

5285e6c4eb4e1a3c60c2103e6819cc2b7af5f2aa
Onion-L <onionl5236@gmail.com>
2024-12-17 23:21:25 +0800

    fix: contextmenu preset

--- preset/safelist/index.ts
+++ preset/safelist/index.ts
@@ -1,9 +1,10 @@
 import { getBtnSafelist } from './button'
+import { contextmenuSafelist } from './contextmenu'
 import { defaultCls } from './default'
 import { IconSafeList } from './icon'
 import { imageSafelist } from './image'
 import { scrollbarSafelist } from './scrollbar'
 
 export function getSafelist(range: number = 20) {
-  return [defaultCls, IconSafeList, imageSafelist, scrollbarSafelist, ...getBtnSafelist(range)]
+  return [defaultCls, IconSafeList, imageSafelist, contextmenuSafelist, scrollbarSafelist, ...getBtnSafelist(range)]
 }

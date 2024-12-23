40da9f66b9ad5d03447e64c2eb7e0fb08a96eb22
Onion-L <onionl5236@gmail.com>
2024-12-23 10:16:45 +0800

    wip: add Marquee component mvp

---
+++ packages/components/marquee/src/marquee.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'OlMarquee',
+})
+</script>
+
+<template>
+  <div class=" flex gap-2 overflow-hidden">
+    <div v-for="i in 4" :key="i" class="flex animate-marquee gap-2 transition-all duration-300 translate-x-[calc(-100%-8px)]">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.animate-marquee {
+  animation: marquee 20s linear infinite;
+}
+
+@keyframes marquee {
+  0% {
+    transform: translateX(calc(-100% - 8px));
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes marquee-reverse {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-100% - 8px));
+  }
+}
+</style>
--- play/src/App.vue
+++ play/src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import { OlProgress } from '@onionl-ui/components'
+import { OlCard, OlCardHeader, OlMarquee } from '@onionl-ui/components'
 </script>
 
 <template>
-  <ol-progress class="w-70 h-2" :value="50" reverse label />
+  <h1>Marquee</h1>
+  <div class="w-screen">
+    <ol-marquee>
+      <ol-card v-for="j in 4" :key="j" class="w-70  flex-shrink-0">
+        <ol-card-header>
+          <div class="flex flex-col gap-2 dark:text-white">
+            <h1 class="text-2xl font-bold m-0">
+              Onionl-UI {{ j }}
+            </h1>
+            <p class="m-0 dark:text-coolGray-3">
+              Onionl-UI is a UI library for Vue 3🚀. It is a library of components for Vue 3.
+            </p>
+          </div>
+        </ol-card-header>
+      </ol-card>
+    </ol-marquee>
+  </div>
 </template>
 
 <style scoped>

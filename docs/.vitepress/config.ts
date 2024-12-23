679461a08983b1edf057ed091a0feed2898226ad
Onion-L <onionl5236@gmail.com>
2024-12-24 00:31:24 +0800

    refactor: change component config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,41 +6,41 @@ const NavLink: DefaultTheme.NavItemWithLink[] = [
   { text: 'Get Started', link: '/guide/getting-started' },
 ]
 
-const BasicComponentItems = [
-  { text: 'Button 按钮', link: '/components/button' },
-  { text: 'Card 卡片', link: '/components/card' },
-  { text: 'Contextmenu 右键菜单', link: '/components/contextmenu' },
+const VisualInteractionItems = [
+  { text: 'FlipCard 翻转卡片', link: '/components/flipCard' },
+  { text: 'Halo 光晕背景', link: '/components/halo' },
   { text: 'Image 图片', link: '/components/image' },
-  { text: 'Input 输入框', link: '/components/input' },
+  { text: 'PixelImage 像素图片', link: '/components/pixelImage' },
+]
+
+const ContentDisplayItems = [
+  { text: 'Card 卡片', link: '/components/card' },
   { text: 'Marquee 跑马灯', link: '/components/marquee' },
+  { text: 'Progress 进度条', link: '/components/progress' },
   { text: 'Scrollbar 滚动条', link: '/components/scrollbar' },
 ]
 
-const DataComponentItems = [
-  { text: 'Progress 进度条', link: '/components/progress' },
-]
-
-const OtherComponentItems = [
-  { text: 'FlipCard 翻转卡片', link: '/components/flipCard' },
-  { text: 'Halo 光晕背景', link: '/components/halo' },
-  { text: 'PixelImage 像素图片', link: '/components/pixelImage' },
+const FormInteractionItems = [
+  { text: 'Button 按钮', link: '/components/button' },
+  { text: 'Contextmenu 右键菜单', link: '/components/contextmenu' },
+  { text: 'Input 输入框', link: '/components/input' },
 ]
 
 const Components = [
   {
     text: 'Basic 基础',
     collapsed: false,
-    items: BasicComponentItems,
+    items: FormInteractionItems,
   },
   {
-    text: 'Data 数据',
+    text: 'Content 内容展示',
     collapsed: false,
-    items: DataComponentItems,
+    items: ContentDisplayItems,
   },
   {
-    text: 'Other 其他',
+    text: 'Visual Interaction 视觉交互',
     collapsed: false,
-    items: OtherComponentItems,
+    items: VisualInteractionItems,
   },
 ]
 

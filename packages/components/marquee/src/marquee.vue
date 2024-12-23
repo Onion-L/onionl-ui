1fe6193adf43b447f0a3c375f21eaa34293485c2
Onion-L <onionl5236@gmail.com>
2024-12-23 11:20:28 +0800

    feat: optimize transform animation

--- packages/components/marquee/src/marquee.vue
+++ packages/components/marquee/src/marquee.vue
@@ -1,25 +1,54 @@
 <script lang="ts" setup>
+import { computed } from 'vue'
+
 defineOptions({
   name: 'OlMarquee',
 })
+
+const props = withDefaults(defineProps<{
+  duration?: number
+  reverse?: boolean
+  gap?: string
+}>(), {
+  duration: 20,
+  reverse: false,
+  gap: '2rem',
+})
+
+const repeat = 4
+const duration = computed(() => `${props.duration}s`)
 </script>
 
 <template>
-  <div class=" flex gap-2 overflow-hidden">
-    <div v-for="i in 4" :key="i" class="flex animate-marquee gap-2 transition-all duration-300 translate-x-[calc(-100%-8px)]">
+  <div class="flex [gap:var(--onl-marquee-gap)] overflow-hidden marquee-variable">
+    <div
+      v-for="i in repeat"
+      :key="i"
+      class="flex [gap:var(--onl-marquee-gap)]"
+      :class="props.reverse ? 'animate-marquee-reverse' : 'animate-marquee'"
+    >
       <slot />
     </div>
   </div>
 </template>
 
 <style scoped>
+.marquee-variable {
+  --onl-marquee-duration: v-bind(duration);
+  --onl-marquee-gap: v-bind(gap);
+}
+
 .animate-marquee {
-  animation: marquee 20s linear infinite;
+  animation: marquee var(--onl-marquee-duration) linear infinite;
+}
+
+.animate-marquee-reverse {
+  animation: marquee-reverse var(--onl-marquee-duration) linear infinite;
 }
 
 @keyframes marquee {
   0% {
-    transform: translateX(calc(-100% - 8px));
+    transform: translateX(calc(-100% - var(--onl-marquee-gap)));
   }
   100% {
     transform: translateX(0);
@@ -31,7 +60,7 @@ defineOptions({
     transform: translateX(0);
   }
   100% {
-    transform: translateX(calc(-100% - 8px));
+    transform: translateX(calc(-100% - var(--onl-marquee-gap)));
   }
 }
 </style>

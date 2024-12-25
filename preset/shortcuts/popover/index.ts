77be0ec34be568f7678f974c7cf28e8056d09e27
Onion-L <onionl5236@gmail.com>
2024-12-25 11:02:18 +0800

    fix: set nowrap for content

--- preset/shortcuts/popover/index.ts
+++ preset/shortcuts/popover/index.ts
@@ -2,7 +2,7 @@ import type { Shortcut } from 'unocss'
 
 export const popoverShortcuts: Shortcut[] = [
   ['ol-popover', 'relative inline-block'],
-  ['ol-popover__content', 'text-popover-text bg-popover p-y-2 p-x-4 border border-solid border-1 border-popover-border rounded-md shadow-md'],
+  ['ol-popover__content', 'text-popover-text bg-popover p-y-2 p-x-4 border border-solid border-1 border-popover-border rounded-md shadow-md whitespace-nowrap'],
   ['ol-popover__content--arrow', 'absolute border border-popover-border w-2 h-2 bg-popover border border-popover-border border-solid rotate-45'],
   ['ol-popover__arrow--top', 'border-l-transparent border-t-transparent'],
   ['ol-popover__arrow--bottom', 'border-r-transparent border-b-transparent'],

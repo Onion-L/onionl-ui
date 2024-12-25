95c89fbe3715ed3a5484bd20be97800ee8d0b2b9
Onion-L <onionl5236@gmail.com>
2024-12-25 16:39:19 +0800

    docs: add Popover component documentation

---
+++ docs/demo/popover/basic.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <ol-popover>
+    <template #trigger>
+      <ol-button>
+        Popover
+      </ol-button>
+    </template>
+    <ol-card class="border-none">
+      <ol-card-header class="flex flex-col gap-2 p-1">
+        <h4 class="!m-0">
+          Buzz Lightyear
+        </h4>
+        <p class="!m-0 text-sm dark:text-gray-300 text-gray-600">
+          To infinity and beyond🚀
+        </p>
+      </ol-card-header>
+    </ol-card>
+  </ol-popover>
+</template>
+
+<style>
+
+</style>
---
+++ docs/demo/popover/duration.vue
@@ -0,0 +1,16 @@
+<template>
+  <ol-popover :duration="2500">
+    <template #trigger>
+      <ol-button>
+        Duration
+      </ol-button>
+    </template>
+    <ol-card class="border-none">
+      <ol-card-header class="flex flex-col gap-2 p-1">
+        <p class="!m-0 text-sm dark:text-gray-300 text-gray-600">
+          我快如闪电
+        </p>
+      </ol-card-header>
+    </ol-card>
+  </ol-popover>
+</template>

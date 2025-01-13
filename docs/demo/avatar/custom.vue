144fe6b8003cb2e89a7cd0cee28dd3e99d3b329f
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-13 13:46:40 +0100

    feat: create custom demo file

---
+++ docs/demo/avatar/custom.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex gap-5">
+    <div v-for="i in [20, 18, 15, 12, 10, 8, 5]" :key="i">
+      <p>Size of {{ i }}</p>
+      <ol-avatar src="https://i.postimg.cc/50sV11jF/pexels-photo-128302.jpg" :size="i" />
+    </div>
+  </div>
+</template>

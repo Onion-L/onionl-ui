aa4def9c05f9a3733e65f48d1a8e9b0c948545b9
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-13 13:46:23 +0100

    feat: create borderRadius demo file

---
+++ docs/demo/avatar/borderRadius.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex gap-4 items-center">
+    <ol-avatar icon="i-mdi-account" border-radius="10" />
+    <ol-avatar icon="i-mdi-account" border-radius="20" />
+    <ol-avatar icon="i-mdi-account" border-radius="30" />
+    <ol-avatar icon="i-mdi-account" border-radius="40" />
+  </div>
+</template>

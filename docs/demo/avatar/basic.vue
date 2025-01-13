cbf25258a1cd73cb04014f0e5ec09a72f9a0cd40
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-13 13:46:04 +0100

    feat: create basic demo file

---
+++ docs/demo/avatar/basic.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex gap-4 items-center">
+    <ol-avatar src="https://i.postimg.cc/50sV11jF/pexels-photo-128302.jpg" alt="User avatar" />
+    <ol-avatar initials="JD" />
+    <ol-avatar icon="i-mi-person" />
+    <ol-avatar fallbackText="User" />
+  </div>
+</template>

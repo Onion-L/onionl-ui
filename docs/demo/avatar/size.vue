0a46087f7e3c4b69478630da3ba3382dc09b4a97
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-13 13:48:09 +0100

    feat: create size demo file

---
+++ docs/demo/avatar/size.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex gap-5">
+    <div v-for="size in ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl']" :key="size">
+      <p>{{ size }}</p>
+      <ol-avatar :size />
+    </div>
+  </div>
+</template>

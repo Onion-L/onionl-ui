883ae878e72139cc5c73621d63abdacee95afd4a
Carlos Segura Garcia <lithos.contact@gmail.com>
2025-01-13 13:45:43 +0100

    feat: create BackgroundColor demo file

---
+++ docs/demo/avatar/backgroundColor.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex gap-4 items-center">
+    <ol-avatar icon="i-mdi-account" background-color="red" />
+    <ol-avatar icon="i-mdi-account" background-color="blue" />
+    <ol-avatar icon="i-mdi-account" background-color="green" />
+    <ol-avatar icon="i-mdi-account" background-color="yellow" />
+  </div>
+</template>

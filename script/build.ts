41307be96e5efae07b07145549643488a92544d0
Onion-L <onionl5236@gmail.com>
2025-01-14 21:08:37 +0800

    fix: update publish workflow

--- script/build.ts
+++ script/build.ts
@@ -2,11 +2,10 @@ import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import glob from 'fast-glob'
-import fs from 'fs-extra'
 import UnoCSS from 'unocss/vite'
 import { build } from 'vite'
 import dts from 'vite-plugin-dts'
-import { buildConfig, onionlPath, pkgPath, rollupOptions, rootPath } from './config-info'
+import { buildConfig, pkgPath, rollupOptions, rootPath } from './config-info'
 
 export function excludeFiles(files: string[]) {
   const excludes = ['node_modules', 'test', 'dist', 'preset']
@@ -14,14 +13,6 @@ export function excludeFiles(files: string[]) {
     const position = path.startsWith(rootPath) ? rootPath.length : 0
     return !excludes.some(exclude => path.includes(exclude, position))
   })
-}
-
-export async function copyFiles() {
-  await Promise.all([
-    fs.copy('README.md', resolve(rootPath, 'dist/README.md')),
-    fs.copy('LICENSE', resolve(rootPath, 'dist/LICENSE')),
-    fs.copy(resolve(onionlPath, 'package.json'), resolve(rootPath, 'dist/package.json')),
-  ])
 }
 
 (async function buildAll() {
@@ -56,6 +47,4 @@ export async function copyFiles() {
         : null],
     })
   })
-
-  await copyFiles()
 })()

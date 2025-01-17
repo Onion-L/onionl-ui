db58cfdca040d3f72a37ae86910a35e102ad514f
Onion-L <onionl5236@gmail.com>
2025-01-17 17:45:39 +0800

    fix: correct avatar safelist

--- preset/safelist/avatar/index.ts
+++ preset/safelist/avatar/index.ts
@@ -4,11 +4,11 @@ export const avatarSafelist = [
   'ol-avatar--square',
   'ol-avatar--clickable',
   // Avatar sizes
-  'ol-avatar-xs',
-  'ol-avatar-sm',
-  'ol-avatar-md',
-  'ol-avatar-lg',
-  'ol-avatar-xl',
-  'ol-avatar-2xl',
-  'ol-avatar-3xl',
+  'ol-avatar--xs',
+  'ol-avatar--sm',
+  'ol-avatar--md',
+  'ol-avatar--lg',
+  'ol-avatar--xl',
+  'ol-avatar--2xl',
+  'ol-avatar--3xl',
 ]

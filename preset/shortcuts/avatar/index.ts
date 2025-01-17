6adaf1cc63c47d359aefa1b939d09d8939875caf
Onion-L <onionl5236@gmail.com>
2025-01-17 21:44:58 +0800

    fix: correct avatar shortcut

--- preset/shortcuts/avatar/index.ts
+++ preset/shortcuts/avatar/index.ts
@@ -6,11 +6,11 @@ export const avatarShortcuts: Shortcut[] = [
   ['ol-avatar--square', 'rounded-md'],
   ['ol-avatar--clickable', 'hover:scale-105 hover:shadow-sm transition-transform duration-200'],
   // Avatar sizes
-  ['ol-avatar-xs', 'w-6 h-6 text-xs'],
-  ['ol-avatar-sm', 'w-8 h-8 text-sm'],
-  ['ol-avatar-md', 'w-10 h-10 text-base'],
-  ['ol-avatar-lg', 'w-12 h-12 text-lg'],
-  ['ol-avatar-xl', 'w-14 h-14 text-xl'],
-  ['ol-avatar-2xl', 'w-16 h-16 text-2xl'],
-  ['ol-avatar-3xl', 'w-18 h-18 text-3xl'],
+  ['ol-avatar--xs', 'w-6 h-6 text-xs'],
+  ['ol-avatar--sm', 'w-8 h-8 text-sm'],
+  ['ol-avatar--md', 'w-10 h-10 text-base'],
+  ['ol-avatar--lg', 'w-12 h-12 text-lg'],
+  ['ol-avatar--xl', 'w-14 h-14 text-xl'],
+  ['ol-avatar--2xl', 'w-16 h-16 text-2xl'],
+  ['ol-avatar--3xl', 'w-18 h-18 text-3xl'],
 ]

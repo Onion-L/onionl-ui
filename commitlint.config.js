da15e5ca861edb7705284d68a795e7d19356850b
Onion-L <onionl5236@gmail.com>
2025-02-22 10:32:26 +1100

    chore: remove redundant comment

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,24 +6,24 @@ export default {
       2,
       'always',
       [
-        'feat', // 新功能
-        'fix', // 修复bug
-        'wip', // 开发中
-        'docs', // 文档修改
-        'style', // 代码格式修改
-        'lint', // lint相关
-        'refactor', // 代码重构
-        'perf', // 优化相关
-        'test', // 测试用例修改
-        'chore', // 其他修改
-        'build', // 编译相关的修改
-        'ci', // CI相关修改
+        'feat',
+        'fix',
+        'wip',
+        'docs',
+        'style',
+        'lint',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'build',
+        'ci',
       ],
     ],
-    'type-case': [2, 'always', 'lower-case'], // type必须小写
-    'type-empty': [2, 'never'], // type不能为空
-    'subject-empty': [2, 'never'], // subject不能为空
-    'subject-full-stop': [2, 'never', '.'], // subject结尾不能有.
-    'header-max-length': [2, 'always', 72], // header最大长度
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
   },
 }
